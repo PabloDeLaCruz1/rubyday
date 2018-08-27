60c4db6e771318562aa65197f5452d28a215a1c4
Pablo De La Cruz <pdelac@ramapo.edu>
2018-08-27 14:26:25 -0400

    learning some ruby

---
+++ basics.rb
@@ -0,0 +1,26 @@
+# 5 + 10
+# result = 1090 - 63
+# second_result = (result / 25).to_s
+# puts "the value of the second result is #{second_result}"
+
+# range = (1..10).to_a
+# puts range
+# puts range.include?(22)
+
+# big_number = 999999
+# small_number = 3
+
+# if big_number < small_number 
+#     puts "Everything is fine in the universe!"
+# end
+
+# def sum(*args)
+#     puts args.reduce(&:+)
+# end
+
+# sum(1,2,4)
+
+colors = ["red", "white", "blue"]
+red, white, blue = colors
+
+puts red
